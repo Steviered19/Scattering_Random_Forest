8a1bbf33a0446085b7cd9848bf83c1d7769f25fc
Steviered19 <51456129+Steviered19@users.noreply.github.com>
2019-07-16 12:50:50 -0700

    Update convert.f90
    Updated file units to utilize newunit function

--- convert.f90
+++ convert.f90
@@ -9,29 +9,30 @@ real :: base_lab_energy, systematic_error, normalization !header line
 real :: lab_energy, scattering_angle, experimental_value, statistical_error!data points
 integer :: collected_data_points !part of header line
 integer :: pp_line, np_line, z !indexes for counting lines
+integer :: np_dat_input, pp_dat_input, np_dat_output, pp_dat_output !file unit identifiers
 
 !open input data files
-open(unit=20, file="npnew3sig.dat.txt", status='unknown')
-open(unit=30, file="ppnew3sig.dat.txt", status='unknown')
+open(newunit=np_dat_input, file="npnew3sig.dat.txt", status='unknown')
+open(newunit=pp_dat_input, file="ppnew3sig.dat.txt", status='unknown')
 
 !open new .csv data files to act as output
-open(unit=40, file="np.dat.csv", status='unknown')
-open(unit=50, file="pp.dat.csv", status='unknown')
+open(newunit=np_dat_output, file="np.dat.csv", status='unknown')
+open(newunit=pp_dat_output, file="pp.dat.csv", status='unknown')
 
 !read input and write to output
 
 !np data
 np_line=1 !initialize at line 1
 do
-    read(20,3,end=25) base_lab_energy, collected_data_points, &
+    read(np_dat_input,3,end=25) base_lab_energy, collected_data_points, &
     & systematic_error, normalization, dummy_string, reaction_type, observable
     if (base_lab_energy > 350.0) exit
-    read(20,*) !bibliographic line
+    read(np_dat_input,*) !bibliographic line
     do z=1, collected_data_points
-        read(20,4) lab_energy, scattering_angle, experimental_value, statistical_error !4 columns of data
-        write(40,5) lab_energy, ', ', scattering_angle, ', ', experimental_value, ', ', &
-            & statistical_error, ', ', reaction_type, ', ', observable, ', ', &
-            & systematic_error, ', ', normalization !ouput to .csv file
+        read(np_dat_input,4) lab_energy, scattering_angle, experimental_value, statistical_error !4 columns of data
+        write(np_dat_output,5) lab_energy, ', ', scattering_angle, ', ', &
+            & experimental_value, ', ', statistical_error, ', ', reaction_type, ', ', &
+            & observable, ', ', systematic_error, ', ', normalization !ouput to .csv file
     end do
 np_line=np_line+1 !move to next data set
 end do
@@ -40,13 +41,13 @@ end do
 !pp data
 pp_line=1 !initialize at line 1
 do
-    read(30,3,end=15) base_lab_energy, collected_data_points, &
+    read(pp_dat_input,3,end=15) base_lab_energy, collected_data_points, &
     systematic_error, normalization, dummy_string, reaction_type, observable
     if (base_lab_energy > 350.0) exit
-    read(30,*) !bibliographic line
+    read(pp_dat_input,*) !bibliographic line
     do z=1, collected_data_points
-        read(30,4) lab_energy, scattering_angle, experimental_value, statistical_error !4 columns of data
-        write(50,5) lab_energy, ', ', scattering_angle, ', ', experimental_value, ', ', &
+        read(pp_dat_input,4) lab_energy, scattering_angle, experimental_value, statistical_error !4 columns of data
+        write(pp_dat_output,5) lab_energy, ', ', scattering_angle, ', ', experimental_value, ', ', &
             & statistical_error, ', ', reaction_type, ', ', observable, ', ', &
             & systematic_error, ', ', normalization !ouput to .csv file
     end do
@@ -62,6 +63,9 @@ end do
 5 format(2(f13.6,a2), 2(f13.5,a2), 2a2, a7, a2, f7.3, a2, f7.3)
 
 !close all files
-close(unit=20); close(unit=30); close(unit=40); close(unit=50)
+close(np_dat_input)
+close(pp_dat_input)
+close(np_dat_output)
+close(pp_dat_output)
 
 end program convert
