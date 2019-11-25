d90101530bf5fa91c24cfa05a30105dfaa5f046c
[Steven] <[sjbradley@sdsu.edu]>
2019-11-25 13:46:05 -0800

    Edited random_convert to write to a combined file as well as separated ones.

--- random_convert.f90
+++ random_convert.f90
@@ -1,22 +1,28 @@
 program converter
   implicit none
   real :: E, k, val, sigma
-  integer :: sgt_input, sgtt_input, sgt_output, sgtt_output
+  integer :: sgt_input, sgtt_input, sgt_output, sgtt_output, total_output
 
   !open input files
   open(newunit=sgt_input, file="random_sgt.dat", status="unknown")
   open(newunit=sgtt_input, file="random_sgtt.dat", status="unknown")
   !open output
+  open(newunit=total_output, file="CSV_files/random.sgt_and_sgtt.csv", status="unknown")
   open(newunit=sgt_output, file="CSV_files/random.sgt.csv", status="unknown")
   open(newunit=sgtt_output, file="CSV_files/random.sgtt.csv", status="unknown")
   !write header line
-  write(sgt_output,*) "E_LAB", ", ", "k_cm", ", ", "exp_val", ", ", "sigma"
-  write(sgtt_output,*) "E_LAB", ", ", "k_cm", ", ", "exp_val", ", ", "sigma"
+  write(total_output,*) "E_LAB", ", ", "k_cm", ", ", "exp_val", ", ", "sigma", ", ",&
+  & "rxn_type", ", ", "observable"
+  write(sgt_output,*) "E_LAB", ", ", "k_cm", ", ", "exp_val", ", ", "sigma", ", ",&
+  & "rxn_type", ", ", "observable"
+  write(sgtt_output,*) "E_LAB", ", ", "k_cm", ", ", "exp_val", ", ", "sigma", ", ",&
+  & "rxn_type", ", ", "observable"
 
   !read and write data
   read(sgt_input,*) !header line
   do
     read(sgt_input, 5, end=25) E, k, val, sigma
+    write(total_output, 6) E, ", ", k, ", ", val, ", ", sigma, ", ", "NP  ", ", ", "SGT "
     write(sgt_output, 6) E, ", ", k, ", ", val, ", ", sigma, ", ", "NP  ", ", ", "SGT "
   enddo
   25 continue
@@ -24,6 +30,7 @@ program converter
   read(sgtt_input,*) !header line
   do
     read(sgtt_input, 5, end=35) E, k, val, sigma
+    write(total_output, 6) E, ", ", k, ", ", val, ", ", sigma, ", ", "NP  ", ", ", "SGTT"
     write(sgtt_output, 6) E, ", ", k, ", ", val, ", ", sigma, ", ", "NP  ", ", ", "SGTT"
   enddo
   35 continue
