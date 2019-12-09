ee7e7c3c7e04bcfa591b6ce606e4bb2e7ad3351f
[Steven] <[sjbradley@sdsu.edu]>
2019-12-08 22:53:33 -0800

    Added back sections that were lost in computer crash. Begun work with random number function on Gaussian distribution.

--- random_convert.f90
+++ random_convert.f90
@@ -11,30 +11,30 @@ program converter
   open(newunit=sgt_output, file="CSV_files/random.sgt.csv", status="unknown")
   open(newunit=sgtt_output, file="CSV_files/random.sgtt.csv", status="unknown")
   !write header line
-  write(total_output,*) "energy", ", ", "k", ", ", "experimental_value", ", ", "sigma", ", ",&
-  & "rxn_type", ", ", "observable"
-  write(sgt_output,*) "energy", ", ", "k", ", ", "experimental_value", ", ", "sigma", ", ",&
-  & "rxn_type", ", ", "observable"
-  write(sgtt_output,*) "energy", ", ", "k", ", ", "experimental_value", ", ", "sigma", ", ",&
-  & "rxn_type", ", ", "observable"
+  write(total_output,*) "energy", ",", "k", ",", "experimental_value", ",", "sigma", ",",&
+  & "reaction_type", ",", "observable"
+  write(sgt_output,*) "energy", ",", "k", ",", "experimental_value", ",", "sigma", ",",&
+  & "reaction_type", ",", "observable"
+  write(sgtt_output,*) "energy", ",", "k", ",", "experimental_value", ",", "sigma", ",",&
+  & "reaction_type", ",", "observable"
 
   !read and write data
   read(sgt_input,*) !header line
   do
     read(sgt_input, 5, end=25) E, k, val, sigma
-    write(total_output, 6) E, ", ", k, ", ", val, ", ", sigma, ", ", "NP  ", ", ", "  SGT "
-    write(sgt_output, 6) E, ", ", k, ", ", val, ", ", sigma, ", ", "NP  ", ", ", "  SGT "
+    write(total_output, 6) E, ",", k, ",", val, ",", sigma, ",", "NP",", ","  SGT "
+    write(sgt_output, 6) E, ",", k, ",", val, ",", sigma, ",", "NP",", ","  SGT "
   enddo
   25 continue
 
   read(sgtt_input,*) !header line
   do
     read(sgtt_input, 5, end=35) E, k, val, sigma
-    write(total_output, 6) E, ", ", k, ", ", val, ", ", sigma, ", ", "NP  ", ", ", "  SGTT"
-    write(sgtt_output, 6) E, ", ", k, ", ", val, ", ", sigma, ", ", "NP  ", ", ", "  SGTT"
+    write(total_output, 6) E, ",", k, ",", val, ",", sigma, ",", "NP",", ","  SGTT"
+    write(sgtt_output, 6) E, ",", k, ",", val, ",", sigma, ",", "NP",", ","  SGTT"
   enddo
   35 continue
 
   5 format(e15.8, 3e21.8)
-  6 format(e15.8, 3(a2, e21.8), a2, a4, a2, a6)
+  6 format(e15.8, 3(a1, e18.8), a1, a2, a1, a6)
 end program converter
