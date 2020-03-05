6834c73104ffc64569b7ed59e4cd1c28bdc2ce2a
[Steven] <[sjbradley@sdsu.edu]>
2020-03-04 20:29:23 -0800

    Minr edits made when creating poster.

--- random_convert.f90
+++ random_convert.f90
@@ -22,19 +22,19 @@ program converter
   read(sgt_input,*) !header line
   do
     read(sgt_input, 5, end=25) E, k, val, sigma
-    write(total_output, 6) E, ",", k, ",", val, ",", sigma, ",", "NP",", ","  SGT "
-    write(sgt_output, 6) E, ",", k, ",", val, ",", sigma, ",", "NP",", ","  SGT "
+    write(total_output, 6) E, ",", k, ",", val, ",", sigma, ",", "NP",", ","   SGT "
+    write(sgt_output, 6) E, ",", k, ",", val, ",", sigma, ",", "NP",", ","   SGT "
   enddo
   25 continue
 
   read(sgtt_input,*) !header line
   do
     read(sgtt_input, 5, end=35) E, k, val, sigma
-    write(total_output, 6) E, ",", k, ",", val, ",", sigma, ",", "NP",", ","  SGTT"
-    write(sgtt_output, 6) E, ",", k, ",", val, ",", sigma, ",", "NP",", ","  SGTT"
+    write(total_output, 6) E, ",", k, ",", val, ",", sigma, ",", "NP",", ","   SGTT"
+    write(sgtt_output, 6) E, ",", k, ",", val, ",", sigma, ",", "NP",", ","   SGTT"
   enddo
   35 continue
 
   5 format(e15.8, 3e21.8)
-  6 format(e15.8, 3(a1, e18.8), a1, a2, a1, a6)
+  6 format(e15.8, 3(a1, e18.8), a1, a2, a1, a7)
 end program converter
